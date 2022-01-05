73198ea50b8edda88b775425ecb82df089699e3f
xxAROX <arox.email.0@gmail.com>
2022-01-05 18:45:31 +0100

    more events!!

---
+++ src/xxAROX/BuildFFA/event/EnterArenaProtectionAreaEvent.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright (c) Jan Sohn
+ * All rights reserved.
+ * I don't want anyone to use my source code without permission.
+ */
+
+declare(strict_types=1);
+namespace xxAROX\BuildFFA\event;
+/**
+ * Class EnterArenaProtectionAreaEvent
+ * @package xxAROX\BuildFFA\event
+ * @author Jan Sohn / xxAROX
+ * @date 05. Januar, 2022 - 18:43
+ * @ide PhpStorm
+ * @project BuildFFA
+ * @description idk, if i use this lol, but here it is!
+ */
+class EnterArenaProtectionAreaEvent extends LeaveArenaProtectionAreaEvent{
+}
---
+++ src/xxAROX/BuildFFA/event/LeaveArenaProtectionAreaEvent.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Copyright (c) Jan Sohn
+ * All rights reserved.
+ * I don't want anyone to use my source code without permission.
+ */
+
+declare(strict_types=1);
+namespace xxAROX\BuildFFA\event;
+use pocketmine\event\Event;
+use xxAROX\BuildFFA\game\Arena;
+use xxAROX\BuildFFA\player\xPlayer;
+
+
+/**
+ * Class LeaveArenaProtectionAreaEvent
+ * @package xxAROX\BuildFFA\event
+ * @author Jan Sohn / xxAROX
+ * @date 05. Januar, 2022 - 18:36
+ * @ide PhpStorm
+ * @project BuildFFA
+ */
+class LeaveArenaProtectionAreaEvent extends Event{
+	public function __construct(protected xPlayer $player, protected Arena $arena){
+	}
+
+	/**
+	 * Function getPlayer
+	 * @return xPlayer
+	 */
+	public function getPlayer(): xPlayer{
+		return $this->player;
+	}
+
+	/**
+	 * Function getArena
+	 * @return Arena
+	 */
+	public function getArena(): Arena{
+		return $this->arena;
+	}
+}
